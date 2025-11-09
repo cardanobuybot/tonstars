2f605d760f032ff5643a6c8d9214f505d70c1eb1
CardanoBuyBot <info.1one@yandex.ru>
2025-11-09 09:54:49 +0100

    Update TonConnectProvider to use static manifestUrl

--- apps/web/app/providers/TonConnectProvider.tsx
+++ apps/web/app/providers/TonConnectProvider.tsx
@@ -7,18 +7,14 @@ import {
   useTonWallet
 } from '@tonconnect/ui-react';
 
-// Реэкспортируем хуки, чтобы их можно было импортировать из этого файла
 export { useTonConnectUI, useTonWallet };
-
-// Корректный manifestUrl для клиентской и серверной среды
-const manifestUrl =
-  typeof window !== 'undefined'
-    ? `${window.location.origin}/.well-known/tonconnect-manifest.json`
-    : '/.well-known/tonconnect-manifest.json';
 
 export default function TonConnectProvider({ children }: { children: React.ReactNode }) {
   return (
-    <TonConnectUIProvider manifestUrl={manifestUrl}>
+    <TonConnectUIProvider
+      manifestUrl="https://tonstars.io/.well-known/tonconnect-manifest.json"
+      actionsConfiguration={{ twaReturnUrl: 'https://tonstars.io' }}
+    >
       {children}
     </TonConnectUIProvider>
   );
